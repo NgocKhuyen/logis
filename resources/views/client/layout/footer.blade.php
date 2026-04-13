5dd38c2eea6990949498ba131bcb4f8fae5ffd93
NgocKhuyen <ngockhuyen2k2@gmail.com>
2026-04-13 20:53:49 +0700

    Chỉnh sửa thông tin Header và Footer

--- resources/views/client/layout/footer.blade.php
+++ resources/views/client/layout/footer.blade.php
@@ -1,65 +1,68 @@
-
-
 <footer id="footer" class="footer dark-background">
 
     <div class="container footer-top">
-      <div class="row gy-4">
-        <div class="col-lg-5 col-md-12 footer-about">
-          <a href="index.html" class="logo d-flex align-items-center">
-            <span class="sitename">Logis</span>
-          </a>
-          <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
-          <div class="social-links d-flex mt-4">
-            <a href=""><i class="bi bi-twitter-x"></i></a>
-            <a href=""><i class="bi bi-facebook"></i></a>
-            <a href=""><i class="bi bi-instagram"></i></a>
-            <a href=""><i class="bi bi-linkedin"></i></a>
-          </div>
-        </div>
+        <div class="row gy-4">
+            <div class="col-lg-5 col-md-12 footer-about">
+                <a href="index.html" class="logo d-flex align-items-center">
+                    <span class="sitename">Logis</span>
+                </a>
+                <p>Chuyên cung cấp dịch vụ dọn dẹp nhà ở, văn phòng uy tín, nhanh chóng và tiết kiệm. Đội ngũ chuyên
+                    nghiệp, phục vụ tận nơi, mang lại không gian sạch sẽ cho bạn.</p>
+                <p class="mt-2">
+                    <strong>Hỗ trợ 24/7 – Phục vụ nhanh chóng trong ngày</strong>
+                </p>
+                <div class="social-links d-flex mt-4">
+                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                    <a href=""><i class="bi bi-facebook"></i></a>
+                    <a href=""><i class="bi bi-instagram"></i></a>
+                    <a href=""><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
 
-        <div class="col-lg-2 col-6 footer-links">
-          <h4>Useful Links</h4>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Terms of service</a></li>
-            <li><a href="#">Privacy policy</a></li>
-          </ul>
-        </div>
+            <div class="col-lg-2 col-6 footer-links">
+                <h4>Liên kết nhanh</h4>
+                <ul>
+                    <li><a href="#">Trang chủ</a></li>
+                    <li><a href="#">Giới thiệu</a></li>
+                    <li><a href="#">Dịch vụ</a></li>
+                    <li><a href="#">Liên hệ</a></li>
+                    <li><a href="#">Chính sách bảo mật</a></li>
+                </ul>
+            </div>
 
-        <div class="col-lg-2 col-6 footer-links">
-          <h4>Our Services</h4>
-          <ul>
-            <li><a href="#">Web Design</a></li>
-            <li><a href="#">Web Development</a></li>
-            <li><a href="#">Product Management</a></li>
-            <li><a href="#">Marketing</a></li>
-            <li><a href="#">Graphic Design</a></li>
-          </ul>
-        </div>
+            <div class="col-lg-2 col-6 footer-links">
+                <h4>Dịch vụ</h4>
+                <ul>
+                    <li><a href="#">Dọn nhà theo giờ</a></li>
+                    <li><a href="#">Dọn nhà trọn gói</a></li>
+                    {{-- <li><a href="#">Product Management</a></li>
+                    <li><a href="#">Marketing</a></li>
+                    <li><a href="#">Graphic Design</a></li> --}}
+                </ul>
+            </div>
 
-        <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-          <h4>Contact Us</h4>
-          <p>A108 Adam Street</p>
-          <p>New York, NY 535022</p>
-          <p>United States</p>
-          <p class="mt-4"><strong>Phone:</strong> <span>+1 5589 55488 55</span></p>
-          <p><strong>Email:</strong> <span>info@example.com</span></p>
-        </div>
+            <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+                <h4>Liên hệ</h4>
+                <p>9/37 Khu phố 24</p>
+                <p>phường Linh Xuân</p>
+                <p>Thành phố Hồ Chí Minh</p>
+                <p class="mt-4"><strong>Phone:</strong> <span>0911258142</span></p>
+                <p><strong>Email:</strong> <span>ngockhuyen2k2@gmail.com</span></p>
+            </div>
 
-      </div>
+        </div>
     </div>
 
     <div class="container copyright text-center mt-4">
-      <p>© <span>Copyright</span> <strong class="px-1 sitename">Logis</strong> <span>All Rights Reserved</span></p>
-      <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you've purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-        Designed by <a href="https://bootstrapmade.com/">Nguyễn Ngọc Khuyến</a> | <a href="https://bootstrapmade.com/tools/">Logis</a>
-      </div>
+        <p>© <span>Copyright</span> <strong class="px-1 sitename">Logis</strong> <span>All Rights Reserved</span></p>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you've purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+            Thiết kế bởi <a href="https://bootstrapmade.com/">Nguyễn Ngọc Khuyến</a> | <a
+                href="https://bootstrapmade.com/tools/">Logis</a>
+        </div>
     </div>
 
-  </footer>
+</footer>
--- resources/views/client/layout/header.blade.php
+++ resources/views/client/layout/header.blade.php
@@ -9,11 +9,11 @@
 
         <nav id="navmenu" class="navmenu">
             <ul>
-                <li><a href="index.html" class="active">Home<br></a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="pricing.html">Pricing</a></li>
-                <li class="dropdown"><a href="#"><span>Dropdown</span> <i
+                <li><a href="index.html" class="active">Trang chủ<br></a></li>
+                <li><a href="about.html">Giới thiệu</a></li>
+                <li><a href="services.html">Dịch vụ</a></li>
+                <li><a href="pricing.html">Giá cả</a></li>
+                {{-- <li class="dropdown"><a href="#"><span>Dropdown</span> <i
                             class="bi bi-chevron-down toggle-dropdown"></i></a>
                     <ul>
                         <li><a href="#">Dropdown 1</a></li>
@@ -31,13 +31,13 @@
                         <li><a href="#">Dropdown 3</a></li>
                         <li><a href="#">Dropdown 4</a></li>
                     </ul>
-                </li>
-                <li><a href="contact.html">Contact</a></li>
+                </li> --}}
+                <li><a href="contact.html">Liên hệ</a></li>
             </ul>
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
 
-        <a class="btn-getstarted" href="get-a-quote.html">Get a Quote</a>
+        <a class="btn-getstarted" href="get-a-quote.html">Liên hệ tư vấn</a>
 
     </div>
 </header>
